81dd71017a0dfa2c694be8ff3d207419cc7df0af
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-04-09 18:09:45 +0900

    add

--- lib/mastodon_cthulhu.rb
+++ lib/mastodon_cthulhu.rb
@@ -1,5 +1,24 @@
 require "mastodon_cthulhu/version"
+require "mastodon_cthulhu/convert"
+require "mastodon_cthulhu/convert_random"
+require "mastodon_cthulhu/convert_lang"
+require "mastodon_cthulhu/statuses_controller"
 
 module MastodonCthulhu
-  # Your code goes here...
+  def self.setup(&proc)
+    # create function for Monkey patch
+    extend self
+    (
+    class << self;
+      self
+    end).module_eval do
+      define_method 'convert_toot', &proc
+      # define_method 'b' do
+      #   p 'b'
+      # end
+    end
+
+    # Monkey patch
+    Api::V1::StatusesController.prepend(ApiV1StatusesControllerPatch)
+  end
 end
